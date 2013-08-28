ad73e3eff6bca69c5a8cd7b01836a509a5425ac5
vicacheung <reg@4i4u.com>
2013-08-28 15:06:35 +0800

    新增获取最新版phpMyAdmin功能

--- debian.sh
+++ debian.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 cur_dir=$(pwd)
 source_dir=$cur_dir/source
-
+WEBROOT="/var/www"
 
 echo " Please enter the server domain name, the default is: $hostname  <"
 echo " Example: sec.ht"
@@ -44,25 +44,27 @@ apt-key add dotdeb.gpg
 cat >> /etc/apt/sources.list<<EOF
 deb http://mirror.us.leaseweb.net/dotdeb/ stable all
 deb-src http://mirror.us.leaseweb.net/dotdeb/ stable all
+deb http://packages.dotdeb.org wheezy all
+deb-src http://packages.dotdeb.org wheezy all
 EOF
 
 apt-get update
 apt-get install -y mysql-server mysql-client 
-apt-get install -y nginx unzip curl
+apt-get install -y nginx elinks unzip curl
 apt-get install -y php5-fpm php5-gd php5-mysql php5-curl
 #php.ini
 /etc/init.d/php5-fpm stop
 sed -i "s#;cgi.fix_pathinfo=1#cgi.fix_pathinfo=1#g" /etc/php5/fpm/php.ini
 sed -i "s#disable_functions =#disable_functions = pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority,dl,exec,passthru,proc_open,proc_close,shell_exec,system#g" /etc/php5/fpm/php.ini
 sed -i 's/short_open_tag = Off/short_open_tag = On/g' /etc/php5/fpm/php.ini
-sed -i "s#;open_basedir =#open_basedir = /tmp/:/var/www/:/proc/#g" /etc/php5/fpm/php.ini
+sed -i "s#;open_basedir =#open_basedir = /tmp/:$WEBROOT/:/proc/#g" /etc/php5/fpm/php.ini
 sed -i "s#expose_php = On#expose_php = Off#g" /etc/php5/fpm/php.ini
 #php-fpm
 rm -rf /etc/php5/fpm/pool.d/www.conf
 mv conf/www.conf /etc/php5/fpm/pool.d/www.conf
 mkdir /var/wwwlogs
 mkdir /var/wwwroot
-mkdir /var/www
+mkdir $WEBROOT
 mkdir /var/run/php5
 mkdir /etc/nginx/host
 mkdir /etc/nginx/host
@@ -74,17 +76,23 @@ cp conf/nginx.conf /etc/nginx/nginx.conf
 cp conf/fastcgi_params /etc/nginx/fastcgi_params
 sed -i "s,sec.ht,$hostname,g" /etc/nginx/nginx.conf
 #URL Rewrite
-mv conf/discuz.conf /etc/nginx
-mv conf/discuzx.conf /etc/nginx
-mv conf/sablog.conf /etc/nginx
-mv conf/wordpress.conf /etc/nginx
 mv conf/none.conf /etc/nginx
-cp conf/index.html /var/www/index.html
-cp conf/prober.php /var/www/p.php
-#phpMyAdmin-3.5.3-all-languages
-unzip conf/phpMyAdmin-3.5.3-all-languages.zip
-mv phpMyAdmin-3.5.3-all-languages /var/www/pma
-chown -R www-data /var/www
+cp conf/index.html $WEBROOT/index.html
+cp conf/prober.php $WEBROOT/p.php
+#Get latest phpMyAdmin.zip
+if [ ! -s phpMyAdmin-*-all-languages.zip ]; then
+	PMA_VERSION=`elinks http://jaist.dl.sourceforge.net/project/phpmyadmin/phpMyAdmin/ | awk -F/ '{print $7F}' | sort -n | grep -iv '-' | tail -1`
+	PMA_LINK="http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/"
+	LATEST_PMA_LINK="${PMA_LINK}${PMA_VERSION}/phpMyAdmin-${PMA_VERSION}-all-languages.zip/download"
+	wget -c ${LATEST_PMA_LINK} -O conf/phpMyAdmin.zip
+	unzip conf/phpMyAdmin.zip
+	mv phpMyAdmin-${PMA_VERSION}-all-languages $WEBROOT/pma
+else
+	PMA_VERSION=`ls phpMyAdmin-*-all-languages.zip | awk -F- '{print $2}'`
+	upzip phpMyAdmin-*-all-languages.zip 
+	mv $WEBROOT/phpMyAdmin-*-all-languages $WEBROOT/pma
+fi
+chown -R www-data $WEBROOT
 chown -R www-data /var/wwwroot
 #add host
 cp conf/add.sh /home/
